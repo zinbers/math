30474eb88fbadfabb6c5b583ea383c9406d5cba8
zhangbing <359565935@qq.com>
2015-08-26 10:10:15 +0800

    new branch hello

---
+++ hello.c
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: zinber
+	> Mail: 
+	> Created Time: 三  8/26 09:57:26 2015
+ ************************************************************************/
+#include<stdio.h>
+#include"hello.h"
+void sayHello(){
+    printf("say hello to yajie");
+}
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "hello.h"
 
 int main()
 {
   int i = 0;
   int j = 0;
   printf("%d %d",i,j);
+  sayHello();
   return 0;
 }
